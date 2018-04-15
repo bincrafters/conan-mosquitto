ed9ce60faae404335d451a4c4171503f166465b8
Uilian Ries <uilianries@gmail.com>
2018-04-15 09:07:08 -0300

    #231 Avoid to build MTd for x86
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- build.py
+++ build.py
@@ -7,7 +7,17 @@ import os
 
 
 if __name__ == "__main__":
+    builder = build_template_default.get_builder()
 
-    shared_option_name = False if os.name == 'nt' else None
-    builder = build_template_default.get_builder(shared_option_name=shared_option_name)
+    # Mosquitto doesn't support MTd for x86
+    if os.name == 'nt':
+        # Mosquitto only support shared lib on Windows
+        builder = build_template_default.get_builder(shared_option_name=False)
+        filtered_builds = []
+        for settings, options, env_vars, build_requires, reference in builder.items:
+            if settings["compiler.runtime"] == "MTd" and settings["arch"] == "x86":
+                continue
+            filtered_builds.append([settings, options, env_vars, build_requires])
+        builder.builds = filtered_builds
+
     builder.run()
