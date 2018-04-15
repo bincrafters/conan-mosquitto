02a752770376ec15ab5310e3d4fbee5f1a6cca67
Uilian Ries <uilianries@gmail.com>
2018-04-15 09:16:10 -0300

    #231 Mosquitto doesn't support x86 + debug
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- build.py
+++ build.py
@@ -7,17 +7,7 @@ import os
 
 
 if __name__ == "__main__":
-    builder = build_template_default.get_builder()
 
-    # Mosquitto doesn't support MTd for x86
-    if os.name == 'nt':
-        # Mosquitto only support shared lib on Windows
-        builder = build_template_default.get_builder(shared_option_name=False)
-        filtered_builds = []
-        for settings, options, env_vars, build_requires, reference in builder.items:
-            if settings["compiler.runtime"] == "MTd" and settings["arch"] == "x86":
-                continue
-            filtered_builds.append([settings, options, env_vars, build_requires])
-        builder.builds = filtered_builds
-
+    shared_option_name = False if os.name == 'nt' else None
+    builder = build_template_default.get_builder(shared_option_name=shared_option_name)
     builder.run()
