15a695809c440b7bc9cd6a14cc27943d0c9cd8c2
Uilian Ries <uilianries@gmail.com>
2018-04-17 19:36:31 -0300

    Support mosquittopp on build
    - Add Mosquittopp option
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- build.py
+++ build.py
@@ -4,10 +4,31 @@
 
 from bincrafters import build_template_default
 import os
+import platform
+import copy
 
+def is_windows():
+    return platform.system() == 'Windows'
+
+def is_linux():
+    return platform.system() == 'Linux'
+
+def add_conan_community():
+    conan_remote_env = "CONAN_REMOTES"
+    conan_community_address = "https://api.bintray.com/conan/conan-community/conan"
+    remote = os.getenv(conan_remote_env, "")
+    if remote:
+        remote += ","
+    remote += conan_community_address
+    os.environ[conan_remote_env] = remote
 
 if __name__ == "__main__":
-
-    shared_option_name = False if os.name == 'nt' else None
+    add_conan_community()
+    shared_option_name = False if is_windows() else None
     builder = build_template_default.get_builder(shared_option_name=shared_option_name)
+    if is_linux():
+        extended_builds = copy.deepcopy(builder)
+        for settings, options, env_vars, build_requires, _ in extended_builds.items:
+            options['mosquitto:with_mosquittopp'] = False
+            builder.add(settings=settings, options=options, env_vars=env_vars, build_requires=build_requires)
     builder.run()
