a54e9e6486abcfd344e4b16fbd369af59a7b877c
Uilian Ries <uilianries@gmail.com>
2018-04-18 20:48:30 -0300

    Remove conan-community from remote
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- build.py
+++ build.py
@@ -3,7 +3,6 @@
 
 
 from bincrafters import build_template_default
-import os
 import platform
 import copy
 
@@ -13,17 +12,7 @@ def is_windows():
 def is_linux():
     return platform.system() == 'Linux'
 
-def add_conan_community():
-    conan_remote_env = "CONAN_REMOTES"
-    conan_community_address = "https://api.bintray.com/conan/conan-community/conan"
-    remote = os.getenv(conan_remote_env, "")
-    if remote:
-        remote += ","
-    remote += conan_community_address
-    os.environ[conan_remote_env] = remote
-
 if __name__ == "__main__":
-    add_conan_community()
     shared_option_name = False if is_windows() else None
     builder = build_template_default.get_builder(shared_option_name=shared_option_name)
     if is_linux():
